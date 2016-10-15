18c670243632164119405eff4dfa8eea40838b86
Jowska <johanna.jaskowska@gmail.com>
2016-10-15 20:39:18 +0200

    Cool water + add video script loop

---
+++ Assets/WEBSITE_CONTENT/video/video.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class video : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+}
